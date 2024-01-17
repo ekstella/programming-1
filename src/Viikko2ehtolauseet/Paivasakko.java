6d0a132121338a8d6e64e4582ae7981bd9d38cee
Ella Ekström <eltsu.ekstrom@gmail.com>
2024-01-17 09:27:11 +0200

    solved paivasakko

---
+++ src/Viikko2ehtolauseet/Paivasakko.java
@@ -0,0 +1,13 @@
+package Viikko2ehtolauseet;
+
+import java.util.Scanner;
+
+public class Paivasakko {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner (System.in);
+        System.out.println("Anna nettotulot: ");
+        int nettotulo = lukija.nextInt();
+        int paivasakkoMaara = (nettotulo - 255) / 60;
+        System.out.println("Nettotuloilla " + nettotulo + " päiväsakko on " + paivasakkoMaara + " euroa");
+    }
+}
