4d1dc4def6b774d2b2a07fd35c4d442877445cd8
Ella Ekström <eltsu.ekstrom@gmail.com>
2024-01-18 17:44:25 +0200

    solved toisto

---
+++ src/Viikko3toistorakenne/Toisto.java
@@ -0,0 +1,11 @@
+package Viikko3toistorakenne;
+
+public class Toisto {
+    public static void main(String[] args) {
+        int number = 100;
+
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
