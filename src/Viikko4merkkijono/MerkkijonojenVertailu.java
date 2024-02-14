1411b1bf411a794191404e20b677dca834fefa0d
Ella Ekström <eltsu.ekstrom@gmail.com>
2024-02-14 20:13:36 +0200

    solved merkkijonojenvertailu

---
+++ src/Viikko4merkkijono/MerkkijonojenVertailu.java
@@ -0,0 +1,21 @@
+package Viikko4merkkijono;
+
+    import java.util.Scanner;
+
+    public class MerkkijonojenVertailu {
+        public static void main(String[] args) {
+            Scanner lukija = new Scanner(System.in);
+    
+            System.out.println("Syötä kaksi merkkijonoa:");
+            String eka = lukija.nextLine();
+            String toka = lukija.nextLine();
+    
+            if (eka.equals(toka)) {
+                System.out.println("Syötetyt merkkijonot olivat samat!");
+            } else {
+                System.out.println("Syötetyt merkkijonot eivät olleet samat!");
+            }
+        }
+    }
+    
+
