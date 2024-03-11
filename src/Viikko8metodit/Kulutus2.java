d5c1be4b531310c957247cdc6359e2e9abd8e728
Ella Ekström <eltsu.ekstrom@gmail.com>
2024-03-11 18:42:34 +0200

    solved kulutus 2

---
+++ src/Viikko8metodit/Kulutus2.java
@@ -0,0 +1,38 @@
+package Viikko8metodit;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Kulutus2 {
+    public static void main(String[] args) {
+        int kilometrit = kysyKilometrit();
+        double tankattu = kysyTankkaus();
+        double kulutus = laskeKulutus(kilometrit, tankattu);
+
+        naytaKulutus(kulutus);
+    }
+
+    public static int kysyKilometrit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Anna ajetut kilometrit: ");
+        int kilometrit = sc.nextInt();
+        return kilometrit;
+    }
+
+    public static int kysyTankkaus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Anna tankattu määrä: ");
+        int tankattu = sc.nextInt();
+        return tankattu;
+    }
+
+    public static double laskeKulutus(int kilometrit, double tankattu) {
+       return tankattu / kilometrit * 100;
+    }
+
+    public static void naytaKulutus(double kulutus) {
+        DecimalFormat dec = new DecimalFormat("0.00");
+        System.out.println("Kulutus/100km on " + dec.format(kulutus) + " litraa");
+    }
+}
+
